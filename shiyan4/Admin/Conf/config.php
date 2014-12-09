89ded7cd64002e13048cb70ebd7d0d60c16e2a58
lianyushengren <18804038158@163.com>
2014-12-09 19:31:30 +0800

    更新后台

--- shiyan4/Admin/Conf/config.php
+++ shiyan4/Admin/Conf/config.php
@@ -7,6 +7,7 @@ return array(
 	'DB_PWD' => '123456',	//设置数据库密码
 	'DB_NAME' => 'task2',	//设置连接的数据库
 	'DB_PREFIX' => 'test_',	//设置数据表名称前缀
+	'USER_AUTH_KEY'   => 'No',			//用户认证识别号
 	//'TMPL_ENGINE_TYPE' => 'PHP',	//设置thinkphp的内置模板引擎
 );
 ?>
--- shiyan4/Admin/Lib/Action/IndexAction.class.php
+++ shiyan4/Admin/Lib/Action/IndexAction.class.php
@@ -1,22 +1,24 @@
 <?php
+
 // 本类由系统自动生成，仅供测试用途
-class IndexAction extends Action {
-   
-	public function index(){
+class IndexAction extends Action 
+{
+	public function index()
+	{
 		$this->searchurl = U('Index/login');
 		$this->verifyurl = U('Index/verif');
 		$this->display();
 	}
 
 
-	//登录验证
-	public function login(){
-
+	// 登录验证
+	public function login()
+	{
 		if (!IS_POST)  halt('页面不存在');
 
-		$username = I('username','','trim');
-		$password = I('password','');
-		$verify = I('code','','md5');
+		$username = I('username', '', 'trim');
+		$password = I('password', '');
+		$verify = I('code', '', 'md5');
 	
 		if ($_SESSION['verify'] != $verify) {
 			$this->error('验证码不正确');
@@ -33,97 +35,38 @@ class IndexAction extends Action {
 			$this->error('账号或密码错误');
 		}
 
-		
-		
-
-		//保存Session
-		session(C('USER_AUTH_KEY'), $user['id']);
-		session('yang_adm_username', $user['name']);
-		
-
-		//超级管理员
-		if (9 == $user['usertype']) {
-			session(C('ADMIN_AUTH_KEY'),true);
-		}
+		// 保存Session
+		session(C('USER_AUTH_KEY'), "Yes");
 
 		import('ORG.Util.RBAC');
-		RBAC::saveAccessList();//静态方法，读取权限放到session
-
+		// 静态方法，读取权限放到session
+		RBAC::saveAccessList();
 
 		//跳转
 		$this->redirect('/Manage/');
-		
-
-
 	}
 
-
-	//退出
-	public function logout() {
-
+	// 退出
+	public function logout()
+	{
 		session_unset();
 		session_destroy();
 		$this->redirect('/Index/index');
 	}
 
+	// 登录验证码
+	public function verify()
+	{
 
-
-	//登录验证码
-	public function verify(){
-
-		//导入ThinkPHP扩展类库
-		//文件位置：./ThinkPHP/Extend/Library/ORG/Util/Image.class.php
-		import('ORG.Util.Image');//导入验证码Image类库
-		return Image::buildImageVerify(4, 1);//直接调用类静态方法
+		// 导入ThinkPHP扩展类库
+		import('ORG.Util.Image');
+		// 直接调用类静态方法
+		return Image::buildImageVerify(4, 1);
 	}
-
-	//js 用户名
-	public function checkusername() {
-		$username = I('username','','trim');
-		$id = I('id', 0,'intval');
-		if (empty($username)) {
-			exit(0);
-		}
-		$user = M('admin')->where(array('username' => $username, 'id' => array('neq' , $id)))->find();
-		if ($user) {
-			echo 1;
-		}else {
-			echo 0;
-		}
-	}
-	//js email
-	public function checkemail() {
-		$email = I('email','','trim');
-		$id = I('id', 0,'intval');
-
-		if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			exit(-1);
-		}
-
-		$user = M('admin')->where(array('email' => $email, 'id' => array('neq' , $id)))->find();
-		if ($user) {
-			echo 1;
-		}else {
-			echo 0;
-		}
-	}
-	//js密码
-	public function checkpassword() {
-		$username = I('username','','trim');		
-		$password = I('password','');
-		if (empty($username) || $password == '') {
-			exit(0);
-		}
-		$user = M('admin')->where(array('username' => $username))->find();
-		if ($user && $user['password'] == get_password($password, $user['encrypt'])) {
-			echo 1;
-		}else {
-			echo 0;
-		}
-	}
-
+	
 	//js验证码
-	public function checkcode() {
+	public function checkcode()
+	{
 		$verify = I('code','','md5');
 	
 		if ($_SESSION['verify'] == $verify) {
@@ -133,38 +76,19 @@ class IndexAction extends Action {
 		}
 	}
 
-
-
-	//RBAC
-
-	//js 角色名
-	public function checkRoleName() {
-		$name = I('name','','trim');
-		$id = I('id', 0,'intval');
+	// js 角色名
+	public function checkRoleName()
+	{
+		$name = I('name', '', 'trim');
+		$id = I('id', 0, 'intval');
 		if (empty($name)) {
 			exit(0);
 		}
 		$data = M('role')->where(array('name' => $name, 'id' => array('neq' , $id)))->find();
 		if ($data) {
 			echo 1;
-		}else {
+		} else {
 			echo 0;
 		}
 	}
-
-	//js 节点名//debug
-	public function checkNodeName() {
-		$name = I('name','','trim');
-		$id = I('id', 0,'intval');
-		if (empty($name)) {
-			exit(0);
-		}
-		$data = M('node')->where(array('name' => $name, 'id' => array('neq' , $id)))->find();
-		if ($data) {
-			echo 1;
-		}else {
-			echo 0;
-		}
-	}
-    
 }
