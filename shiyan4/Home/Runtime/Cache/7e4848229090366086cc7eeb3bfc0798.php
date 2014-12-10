2fbac613fafa92a2fb3dfcb56899a23f732ff31d
zsl <1002832816@qq.com>
2014-12-10 14:52:57 +0800

    更改页面名称

--- shiyan4/Home/Runtime/Cache/7e4848229090366086cc7eeb3bfc0798.php
+++ shiyan4/Home/Runtime/Cache/7e4848229090366086cc7eeb3bfc0798.php
@@ -27,12 +27,19 @@
 	<div class="xbox">
 	<form id="SearchForm" name="SearchForm" method="post" action="<?php echo ($searchurl); ?>">
 	<ul class="searchFormDiv">
-	<li>
-	  <select name="modelid">
-	  	<?php if(is_array($method_list)): foreach($method_list as $key=>$vo): ?><option value="<?php echo ($vo["id"]); ?>" ><?php echo (str_replace('查找','',$vo["method"])); ?></option><?php endforeach; endif; ?>
+	<li>公司名：
+	  <select name="modelid1">
+	  	<option value = "">请选择公司名</option>
+	  	<?php if(is_array($c)): foreach($c as $key=>$vo): ?><option value="<?php echo ($vo["id"]); ?>" ><?php echo ($vo["c_name"]); ?></option><?php endforeach; endif; ?>
 	   </select>
 	</li>
-	<li>
+	<li>部门名：
+	  <select name="modelid2">
+	  	<option value = "">请选择部门名</option>
+	  	<?php if(is_array($p)): foreach($p as $key=>$vo): ?><option value="<?php echo ($vo["id"]); ?>" ><?php echo ($vo["p_name"]); ?></option><?php endforeach; endif; ?>
+	   </select>
+	</li>
+	<li>需要查找人员的关键字：
 	  <input name="keyword" type="text" id="keyword"  value="<?php if(empty($keyword)): ?>请输入关键词<?php else: echo ($keyword); endif; ?>" onFocus="if(this.value=='请输入关键词'){this.value='';}" onBlur="if(this.value==''){this.value='请输入关键词';}" />
 	</li>
 	<li>
@@ -48,8 +55,21 @@
 	
 	<ul >
 	<?php if(empty($list)): ?><div>对不起，暂时没有员工的信息！</div>
-	<?php else: ?>	
-	<?php if(is_array($list)): foreach($list as $key=>$v): ?><li><span><?php echo ($v["id"]); ?></span><span><?php echo ($v["name"]); ?></span><span><?php echo ($v["p_name"]); ?></span><span><?php echo ($v["c_com"]); ?></span></li><?php endforeach; endif; endif; ?>
+	<?php else: ?>
+	<table>
+		<tr>
+				<td width = "30px">ID</td>
+				<td width = "80px">员工姓名</td>
+				<td width = "80px">部门名称</td>
+				<td width = "80px">公司名称</td>
+		</tr>
+	<?php if(is_array($list)): foreach($list as $key=>$v): ?><tr>
+			<td><?php echo ($v["id"]); ?></td>
+			<td><?php echo ($v["name"]); ?></td>
+			<td><?php echo ($v["p_name"]); ?></td>
+			<td><?php echo ($v["c_name"]); ?></td>
+		</tr><?php endforeach; endif; ?>
+	</table><?php endif; ?>
 	
 	</ul>
 
